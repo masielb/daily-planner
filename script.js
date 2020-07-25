6370b0e9546ab24a65b39bea738e664b8b54983f
Masiel Bautista <masiel5@users.noreply.github.com>
2020-07-25 19:45:59 -0400

    finished javascript

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
       localStorage.setItem(time, value);
     });
   
+    ///LOOP /// 
     function hourUpdater() {
       // get current number of hours
       var currentHour = moment().hours();
@@ -33,28 +34,52 @@ $(document).ready(function() {
         // if the current hour is greater than the block hour
         if (blockHour < currentHour) {
           // then add class "past"
-          $("this").addClass("past");
-        }
-        
-        // if they are equal
-        if (blockHour === currentHour) {
+          $(this).addClass("past");
+          //else if
+        } else if (blockHour === currentHour) {
           // then remove class "past" and add class "present"
-          $("this").removeClass("past").addClass("present");
+          $(this).removeClass("past").addClass("present");
         } else { // else remove class "past", remove class "present", add class "future"
-          $("this").removeClass("past present").addClass("future");
+          $(this).removeClass("past present").addClass("future");
         }
       });
     }
-  
+
+    //calls the hourUpdater function
     hourUpdater();
     
     // set up interval to check if current time needs to be updated
-    setInterval(function() {hourUpdater}, 15000);
     // which means execute hourUpdater function every 15 seconds
-    // load any saved data from localStorage
-
+    setInterval(function() {hourUpdater();}, 15000);
     
-  
+    // load any saved data from localStorage
+    var hour9 = localStorage.getItem("hour-9");
+    $("#hour-9 .description").val(hour9);
+    
+    var hour10 = localStorage.getItem("hour-10");
+    $("#hour-10 .description").val(hour10);
+
+    var hour11 = localStorage.getItem("hour-11");
+    $("#hour-11 .description").val(hour11);
+
+    var hour12 = localStorage.getItem("hour-12");
+    $("#hour-12 .description").val(hour12);
+
+    var hour13 = localStorage.getItem("hour-13");
+    $("#hour-13 .description").val(hour13);
+
+    var hour14 = localStorage.getItem("hour-14");
+    $("#hour-14 .description").val(hour14);
+
+    var hour15 = localStorage.getItem("hour-15");
+    $("#hour-15 .description").val(hour15);
+
+    var hour16 = localStorage.getItem("hour-16");
+    $("#hour-16 .description").val(hour16);
+
+    var hour17 = localStorage.getItem("hour-17");
+    $("#hour-17 .description").val(hour17);
+    
     // display current day on page
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
   });
